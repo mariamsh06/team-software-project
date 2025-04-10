c77cc3fe40a6fe8b4b720578840444c034fe6844
mariam <mariam2006sherif@gmail.com>
2025-04-10 19:16:50 +0200

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,9 +1,25 @@
 from . import app
 import requests
 from flask import render_template, request
+from datetime import datetime
 
-@app.route('/')
-def index():
+
+
+from flask import render_template
+from . import app  
+
+@app.route('/') # THIS IS OUR ROOOT DIRECTORYYY!! RESERVED FOR OUR HOMEPAGE
+def home():
+    
+    return render_template('homepage.html') # here this just displays our front end for homepage
+
+
+
+
+
+@app.route('/events')  # THIS IS PAGE TWO!! THE EVENTS PAGE
+def events_page():
+    
     url = "https://api.predicthq.com/v1/events/?within=30km@30.0444,31.2357&country=EG"
     headers = {
         "Authorization": "Bearer 5b52pFPahJmUK2XRA4kuH_Yrr8Y9Rjklovb1zhbb"
@@ -11,15 +27,22 @@ def index():
 
     response = requests.get(url, headers=headers)
 
-    print("Status Code:", response.status_code)
-    print("Response Text:", response.text)
-
     if response.status_code == 200:
-        events = response.json()
-        return render_template("index.html", events=events["results"])
- 
+        events = response.json()["results"]
+        
+       
+        for event in events:
+            
+            event['start'] = datetime.strptime(event['start'], '%Y-%m-%dT%H:%M:%SZ').strftime('%d/%m/%Y')
+            
+            
+            event['location'] = "Cairo"  
+
+        return render_template("events.html", events=events)
     else:
         return f"Error: Unable to fetch events, Status Code: {response.status_code}", 500
+
+
 
 
 
